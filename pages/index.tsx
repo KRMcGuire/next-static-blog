e4af8bb1086b0e311c92d54c11aef9734378d11f
Kieran McGuire <kmcguire@getchalk.com>
2019-12-06 16:01:40 -0400

    Replace the default index with a new tsx one

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Home = () => {
+  return (
+    <>
+      <head>
+        <title>Hello New Blog!</title>
+      </head>
+      <h1>Hello New Blog!</h1>
+    </>
+  );
+}
+
+export default Home;
